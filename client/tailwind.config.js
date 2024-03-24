fe70cb45c34d0d1ae73c333417ac04e3f7071d05
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-03-24 17:53:40 +0530

    bug fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#F5385D',
+
+      }
+    },
   },
   plugins: [],
 }
-
